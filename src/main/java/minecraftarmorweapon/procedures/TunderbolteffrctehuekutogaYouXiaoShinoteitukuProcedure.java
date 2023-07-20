5fbe3899bb5d2937fed687f8ea06dc10c9b93f5a
0526ngs <78349683+hrmcngs@users.noreply.github.com>
2023-07-20 22:14:53 +0900

    version up

--- src/main/java/minecraftarmorweapon/procedures/TunderbolteffrctehuekutogaYouXiaoShinoteitukuProcedure.java
+++ src/main/java/minecraftarmorweapon/procedures/TunderbolteffrctehuekutogaYouXiaoShinoteitukuProcedure.java
@@ -13,7 +13,7 @@ public class TunderbolteffrctehuekutogaYouXiaoShinoteitukuProcedure {
 			Entity _ent = entity;
 			if (!_ent.level.isClientSide() && _ent.getServer() != null) {
 				_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(), _ent.level instanceof ServerLevel ? (ServerLevel) _ent.level : null, 4,
-						_ent.getName().getString(), _ent.getDisplayName(), _ent.level.getServer(), _ent), "/particle dust 1.000 0.969 0.059 1 ~ ~1 ~ 2 1 2 1 0 normal");
+						_ent.getName().getString(), _ent.getDisplayName(), _ent.level.getServer(), _ent), "/particle dust 1.000 0.969 0.059 1 ~ ~1 ~ 0.1 0.1 0.1 1 25");
 			}
 		}
 	}
