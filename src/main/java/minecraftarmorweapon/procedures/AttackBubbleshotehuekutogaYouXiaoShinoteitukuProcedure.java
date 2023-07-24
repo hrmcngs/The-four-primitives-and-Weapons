546c0a3cb5a005a2e0ccf61fca51857c61acd6ab
0526ngs <78349683+hrmcngs@users.noreply.github.com>
2023-07-24 21:47:12 +0900

    version up

--- src/main/java/minecraftarmorweapon/procedures/AttackBubbleshotehuekutogaYouXiaoShinoteitukuProcedure.java
+++ src/main/java/minecraftarmorweapon/procedures/AttackBubbleshotehuekutogaYouXiaoShinoteitukuProcedure.java
@@ -1,9 +1,14 @@
 package minecraftarmorweapon.procedures;
 
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.CommandSource;
+
+import minecraftarmorweapon.init.MinecraftArmorWeaponModMobEffects;
 
 public class AttackBubbleshotehuekutogaYouXiaoShinoteitukuProcedure {
 	public static void execute(Entity entity) {
@@ -16,5 +21,9 @@ public class AttackBubbleshotehuekutogaYouXiaoShinoteitukuProcedure {
 						_ent.getName().getString(), _ent.getDisplayName(), _ent.level.getServer(), _ent), "/particle dust 0.000 0.031 1.000 1 ~ ~1 ~ 0.1 0.1 0.1 1 25");
 			}
 		}
+		if (entity instanceof LivingEntity _entity && !_entity.level.isClientSide())
+			_entity.addEffect(new MobEffectInstance(MobEffects.WEAKNESS,
+					entity instanceof LivingEntity _livEnt && _livEnt.hasEffect(MinecraftArmorWeaponModMobEffects.ATARUTO.get()) ? _livEnt.getEffect(MinecraftArmorWeaponModMobEffects.ATARUTO.get()).getDuration() : 0,
+					entity instanceof LivingEntity _livEnt && _livEnt.hasEffect(MinecraftArmorWeaponModMobEffects.ATARUTO.get()) ? _livEnt.getEffect(MinecraftArmorWeaponModMobEffects.ATARUTO.get()).getAmplifier() : 0, true, false));
 	}
 }
