16f1e34330d312bf628e7429a91956cb61960ead
0526ngs <78349683+hrmcngs@users.noreply.github.com>
2023-11-30 17:03:14 +0900

    versin up

---
+++ models/mojmap-1.19.x/Modeldoragon_leprica_armor_layer_3.java
@@ -0,0 +1,58 @@
+// Made with Blockbench 4.8.3
+// Exported for Minecraft version 1.17 or later with Mojang mappings
+// Paste this class into your mod and generate all required imports
+
+public class Modeldoragon_leprica_armor_layer_3<T extends Entity> extends EntityModel<T> {
+	// This layer location should be baked with EntityRendererProvider.Context in
+	// the entity renderer and passed into this model's constructor
+	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(
+			new ResourceLocation("modid", "doragon_leprica_armor_layer_3"), "main");
+	private final ModelPart head;
+
+	public Modeldoragon_leprica_armor_layer_3(ModelPart root) {
+		this.head = root.getChild("head");
+	}
+
+	public static LayerDefinition createBodyLayer() {
+		MeshDefinition meshdefinition = new MeshDefinition();
+		PartDefinition partdefinition = meshdefinition.getRoot();
+
+		PartDefinition head = partdefinition.addOrReplaceChild("head",
+				CubeListBuilder.create().texOffs(0, 0)
+						.addBox(-4.0F, -7.0F, -4.0F, 8.0F, 8.0F, 8.0F, new CubeDeformation(0.75F)).texOffs(35, 30)
+						.addBox(-3.0F, -6.0F, -8.5F, 6.0F, 7.0F, 3.0F, new CubeDeformation(0.75F)).texOffs(0, 48)
+						.addBox(-4.0F, -7.0F, -4.0F, 8.0F, 8.0F, 8.0F, new CubeDeformation(1.0F)),
+				PartPose.offset(0.0F, 0.0F, 0.0F));
+
+		PartDefinition cube_r1 = head.addOrReplaceChild("cube_r1",
+				CubeListBuilder.create().texOffs(24, 0)
+						.addBox(-3.5F, -26.0F, 18.25F, 2.0F, 2.0F, 6.0F, new CubeDeformation(0.0F)).texOffs(24, 0)
+						.addBox(1.5F, -26.0F, 18.25F, 2.0F, 2.0F, 6.0F, new CubeDeformation(0.0F)),
+				PartPose.offsetAndRotation(0.0F, 24.0F, 0.0F, 0.6545F, 0.0F, 0.0F));
+
+		PartDefinition cube_r2 = head
+				.addOrReplaceChild("cube_r2",
+						CubeListBuilder.create().texOffs(34, 0).addBox(1.6F, -29.25F, 4.0F, 2.0F, 2.0F, 4.0F,
+								new CubeDeformation(0.0F)),
+						PartPose.offsetAndRotation(0.0F, 24.0F, 0.0F, 0.0F, 0.3491F, 0.0F));
+
+		PartDefinition cube_r3 = head.addOrReplaceChild("cube_r3",
+				CubeListBuilder.create().texOffs(34, 0).addBox(-3.6F, -29.25F, 4.0F, 2.0F, 2.0F, 4.0F,
+						new CubeDeformation(0.0F)),
+				PartPose.offsetAndRotation(0.0F, 24.0F, 0.0F, 0.0F, -0.3491F, 0.0F));
+
+		return LayerDefinition.create(meshdefinition, 64, 64);
+	}
+
+	@Override
+	public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay,
+			float red, float green, float blue, float alpha) {
+		head.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+	}
+
+	public void setupAnim(T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw,
+			float headPitch) {
+		this.head.yRot = netHeadYaw / (180F / (float) Math.PI);
+		this.head.xRot = headPitch / (180F / (float) Math.PI);
+	}
+}
