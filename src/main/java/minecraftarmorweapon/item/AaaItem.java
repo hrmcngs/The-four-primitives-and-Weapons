c212c17d1bd0426b936e781409f36a4957c83ac0
0526ngs <78349683+hrmcngs@users.noreply.github.com>
2024-01-08 12:01:25 +0900

    version up

--- src/main/java/minecraftarmorweapon/item/AaaItem.java
+++ src/main/java/minecraftarmorweapon/item/AaaItem.java
@@ -5,9 +5,7 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.Entity;
 
 import minecraftarmorweapon.procedures.AaaYoukuritukusitatokiProcedure;
 
@@ -15,18 +13,13 @@ import minecraftarmorweapon.init.MinecraftArmorWeaponModTabs;
 
 public class AaaItem extends Item {
 	public AaaItem() {
-		super(new Item.Properties().tab(MinecraftArmorWeaponModTabs.TAB_MAGIC_BOOKS).stacksTo(64).rarity(Rarity.COMMON));
+		super(new Item.Properties().tab(MinecraftArmorWeaponModTabs.TAB_YOPKEINAMONO).stacksTo(64).rarity(Rarity.COMMON));
 	}
 
 	@Override
-	public InteractionResultHolder<ItemStack> use(Level world, Player entity, InteractionHand hand) {
-		InteractionResultHolder<ItemStack> ar = super.use(world, entity, hand);
-		ItemStack itemstack = ar.getObject();
-		double x = entity.getX();
-		double y = entity.getY();
-		double z = entity.getZ();
-
-		AaaYoukuritukusitatokiProcedure.execute(world, x, y, z);
-		return ar;
+	public void inventoryTick(ItemStack itemstack, Level world, Entity entity, int slot, boolean selected) {
+		super.inventoryTick(itemstack, world, entity, slot, selected);
+		if (selected)
+			AaaYoukuritukusitatokiProcedure.execute(entity);
 	}
 }
--- src/main/java/minecraftarmorweapon/procedures/AaaYoukuritukusitatokiProcedure.java
+++ src/main/java/minecraftarmorweapon/procedures/AaaYoukuritukusitatokiProcedure.java
@@ -1,14 +1,20 @@
 package minecraftarmorweapon.procedures;
 
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.CommandSource;
 
 public class AaaYoukuritukusitatokiProcedure {
-	public static void execute(LevelAccessor world, double x, double y, double z) {
-		world.setBlock(new BlockPos(x, y - 1, z), Blocks.END_STONE.defaultBlockState(), 3);
-		world.setBlock(new BlockPos(x, y - 2, z), Blocks.END_STONE.defaultBlockState(), 3);
-		world.setBlock(new BlockPos(x, y - 3, z), Blocks.END_STONE.defaultBlockState(), 3);
-		world.setBlock(new BlockPos(x, y - 4, z), Blocks.END_STONE.defaultBlockState(), 3);
+	public static void execute(Entity entity) {
+		if (entity == null)
+			return;
+		{
+			Entity _ent = entity;
+			if (!_ent.level.isClientSide() && _ent.getServer() != null) {
+				_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(), _ent.level instanceof ServerLevel ? (ServerLevel) _ent.level : null, 4,
+						_ent.getName().getString(), _ent.getDisplayName(), _ent.level.getServer(), _ent), "/fill ~3 ~3 ~3 ~-3 ~-3 ~-3 minecraft:end_stone");
+			}
+		}
 	}
 }
