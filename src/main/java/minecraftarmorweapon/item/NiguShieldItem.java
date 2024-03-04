ad57ba05b3750ed707d8609a5d67a6c08664f02f
0526ngs <78349683+hrmcngs@users.noreply.github.com>
2024-03-04 22:13:14 +0900

    version up

--- src/main/java/minecraftarmorweapon/item/NiguShieldItem.java
+++ src/main/java/minecraftarmorweapon/item/NiguShieldItem.java
@@ -1,28 +1,6 @@
-//
-//package minecraftarmorweapon.item;
-//
-//import net.minecraft.world.item.UseAnim;
-//import net.minecraft.world.item.Rarity;
-//import net.minecraft.world.item.ItemStack;
-//import net.minecraft.world.item.Item;
-////import net.minecraft.world.item.CreativeModeTab
-//import minecraftarmorweapon.init.MinecraftArmorWeaponModTabs;
-//import net.minecraft.world.item.ShieldItem;
-//
-//public class NiguShieldItem() {
-//		super(new Item.Properties().tab(MinecraftArmorWeaponModTabs.TAB_WEAPON).stacksTo(1).fireResistant().rarity(Rarity.COMMON));
-//	}
-//
-//	@Override
-//	public UseAnim getUseAnimation(ItemStack itemstack) {
-//		return UseAnim.BLOCK;
-//	}
-//
-//	@Override
-//	public int getEnchantmentValue()
-//	{return 5;
-//	}
-//}
+
+package minecraftarmorweapon.item;
+
 import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.ItemStack;
@@ -47,6 +25,3 @@ public class NiguShieldItem extends ShieldItem {
 	{return 5;
 	}
 }
-
-
-
