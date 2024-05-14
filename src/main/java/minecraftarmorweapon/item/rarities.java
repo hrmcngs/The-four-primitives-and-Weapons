963b163be5d027766cb415812b52c60278eda968
0526ngs <78349683+hrmcngs@users.noreply.github.com>
2024-05-14 16:37:01 +0900

    version up

--- src/main/java/minecraftarmorweapon/item/rarities.java
+++
@@ -1,8 +0,0 @@
-// package minecraftarmorweapon.item;
-// import net.minecraft.item.Rarity;
-// import net.minecraft.util.textFormatting;
-// public class rarities {
-
-// 	public static final Rarity NIGU = Rarity.create( "NIGU", textFormatting.RED );
-// }
-//BoggedOuterQuiverInventory
