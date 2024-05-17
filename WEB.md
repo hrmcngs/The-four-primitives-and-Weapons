f7e891bf9b304f187f164166837b3b8767782123
0526ngs <78349683+hrmcngs@users.noreply.github.com>
2024-05-17 19:27:57 +0900

    version up

---
+++ WEB.md
@@ -0,0 +1,6 @@
+![Anurag's GitHub stats](https://github-readme-stats.vercel.app/api?username=hrmcngs&show_icons=true&theme=dark)
+
+![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=hrmcngs&layout=compact)
+
+https://github.com/GitPois1x/DragonLoot/tree/1.20/src/main/resources/assets/dragonloot/textures
+dorago Armorのtextureはここからダウンロードしました
--- src/main/java/minecraftarmorweapon/procedures/GuardposiyonnoXiaoGuogaKaiShiShiYongsaretatokiProcedure.java
+++ src/main/java/minecraftarmorweapon/procedures/GuardposiyonnoXiaoGuogaKaiShiShiYongsaretatokiProcedure.java
@@ -28,6 +28,18 @@ public class GuardposiyonnoXiaoGuogaKaiShiShiYongsaretatokiProcedure {
 			}
 		}
 		entity.getPersistentData().putDouble("minecraft_armor_weapon:muteki_x_chuzume", (entity.getX()));
+		if (entity.getPersistentData().getBoolean("minecraft_armor_weapon:muteki_weakenss_chuzume_copy") == true) {
+			entity.getPersistentData().putBoolean("minecraft_armor_weapon:muteki_weakenss_chuzume_copy", false);
+			if (entity instanceof LivingEntity _entity && !_entity.level.isClientSide())
+				_entity.addEffect(new MobEffectInstance(MobEffects.WEAKNESS, (int) entity.getPersistentData().getDouble("minecraft_armor_weapon:muteki_weakenss_chuzume_copy_tick"),
+						(int) entity.getPersistentData().getDouble("minecraft_armor_weapon:muteki_weakenss_chuzume_copy_level"), false, false));
+		}
+		if (entity.getPersistentData().getBoolean("minecraft_armor_weapon:muteki_resistance_chuzume_copy") == true) {
+			entity.getPersistentData().putBoolean("minecraft_armor_weapon:muteki_resistance_chuzume_copy", false);
+			if (entity instanceof LivingEntity _entity && !_entity.level.isClientSide())
+				_entity.addEffect(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, (int) entity.getPersistentData().getDouble("minecraft_armor_weapon:muteki_resistance_chuzume_copy_tick"),
+						(int) entity.getPersistentData().getDouble("minecraft_armor_weapon:muteki_resistance_chuzume_copy_level"), false, false));
+		}
 		entity.getPersistentData().putDouble("minecraft_armor_weapon:muteki_y_chuzume", (entity.getY()));
 		entity.getPersistentData().putDouble("minecraft_armor_weapon:muteki_z_chuzume", (entity.getZ()));
 		entity.getPersistentData().putDouble("minecraft_armor_weapon:muteki_knockback_resistance", ((LivingEntity) entity).getAttribute(net.minecraft.world.entity.ai.attributes.Attributes.KNOCKBACK_RESISTANCE).getBaseValue());
@@ -47,9 +59,8 @@ public class GuardposiyonnoXiaoGuogaKaiShiShiYongsaretatokiProcedure {
 		if (entity instanceof LivingEntity _entity && !_entity.level.isClientSide())
 			_entity.addEffect(new MobEffectInstance(MobEffects.WEAKNESS,
 					entity instanceof LivingEntity _livEnt && _livEnt.hasEffect(MinecraftArmorWeaponModMobEffects.GUARD.get()) ? _livEnt.getEffect(MinecraftArmorWeaponModMobEffects.GUARD.get()).getDuration() : 0, 10, true, false));
-		if (entity instanceof LivingEntity _entity && !_entity.level.isClientSide())
-			_entity.addEffect(new MobEffectInstance(MobEffects.LEVITATION,
-					entity instanceof LivingEntity _livEnt && _livEnt.hasEffect(MinecraftArmorWeaponModMobEffects.GUARD.get()) ? _livEnt.getEffect(MinecraftArmorWeaponModMobEffects.GUARD.get()).getDuration() : 0, 255, true, false));
+		if (entity instanceof LivingEntity _entity)
+			_entity.removeEffect(MobEffects.LEVITATION);
 		MinecraftArmorWeaponMod.queueServerWork(entity instanceof LivingEntity _livEnt && _livEnt.hasEffect(MinecraftArmorWeaponModMobEffects.GUARD.get()) ? _livEnt.getEffect(MinecraftArmorWeaponModMobEffects.GUARD.get()).getDuration() : 0, () -> {
 			{
 				Entity _ent = entity;
@@ -59,6 +70,20 @@ public class GuardposiyonnoXiaoGuogaKaiShiShiYongsaretatokiProcedure {
 				}
 			}
 			((LivingEntity) entity).getAttribute(net.minecraft.world.entity.ai.attributes.Attributes.KNOCKBACK_RESISTANCE).setBaseValue((entity.getPersistentData().getDouble("minecraft_armor_weapon:muteki_knockback_resistance")));
+			if (entity instanceof LivingEntity _livEnt ? _livEnt.hasEffect(MobEffects.DAMAGE_RESISTANCE) : false) {
+				entity.getPersistentData().putBoolean("minecraft_armor_weapon:muteki_resistance_chuzume_copy", true);
+				entity.getPersistentData().putDouble("minecraft_armor_weapon:muteki_resistance_chuzume_copy_level",
+						(entity instanceof LivingEntity _livEnt && _livEnt.hasEffect(MobEffects.DAMAGE_RESISTANCE) ? _livEnt.getEffect(MobEffects.DAMAGE_RESISTANCE).getAmplifier() : 0));
+				entity.getPersistentData().putDouble("minecraft_armor_weapon:muteki_resistance_chuzume_copy_tick",
+						(entity instanceof LivingEntity _livEnt && _livEnt.hasEffect(MobEffects.DAMAGE_RESISTANCE) ? _livEnt.getEffect(MobEffects.DAMAGE_RESISTANCE).getDuration() : 0));
+			}
+			if (entity instanceof LivingEntity _livEnt ? _livEnt.hasEffect(MobEffects.WEAKNESS) : false) {
+				entity.getPersistentData().putBoolean("minecraft_armor_weapon:muteki_weakenss_chuzume_copy", true);
+				entity.getPersistentData().putDouble("minecraft_armor_weapon:muteki_weakenss_chuzume_copy_level",
+						(entity instanceof LivingEntity _livEnt && _livEnt.hasEffect(MobEffects.WEAKNESS) ? _livEnt.getEffect(MobEffects.WEAKNESS).getAmplifier() : 0));
+				entity.getPersistentData().putDouble("minecraft_armor_weapon:muteki_weakenss_chuzume_copy_tick",
+						(entity instanceof LivingEntity _livEnt && _livEnt.hasEffect(MobEffects.WEAKNESS) ? _livEnt.getEffect(MobEffects.WEAKNESS).getDuration() : 0));
+			}
 		});
 	}
 }
