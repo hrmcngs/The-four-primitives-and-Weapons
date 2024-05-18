4d7020fe0a31bd3a03ac20ac83ffcb7aff7e6cd5
0526ngs <78349683+hrmcngs@users.noreply.github.com>
2024-05-18 14:51:14 +0900

    version up

--- src/main/java/minecraftarmorweapon/procedures/GuardehuekutogaYouXiaoShinoteitukuProcedure.java
+++ src/main/java/minecraftarmorweapon/procedures/GuardehuekutogaYouXiaoShinoteitukuProcedure.java
@@ -4,7 +4,6 @@ import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.Vec2;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.network.chat.Component;
 import net.minecraft.commands.CommandSourceStack;
@@ -19,11 +18,10 @@ public class GuardehuekutogaYouXiaoShinoteitukuProcedure {
 					"execute as @e[tag=minecraft_armor_weapon_guard_bind] at @s run tp @s ~ ~ ~ ~40 ~");
 		{
 			Entity _ent = entity;
-			_ent.teleportTo((entity.getPersistentData().getDouble("minecraft_armor_weapon:muteki_x_chuzume")), (entity.getPersistentData().getDouble("minecraft_armor_weapon:muteki_y_chuzume")),
-					(entity.getPersistentData().getDouble("minecraft_armor_weapon:muteki_z_chuzume")));
-			if (_ent instanceof ServerPlayer _serverPlayer)
-				_serverPlayer.connection.teleport((entity.getPersistentData().getDouble("minecraft_armor_weapon:muteki_x_chuzume")), (entity.getPersistentData().getDouble("minecraft_armor_weapon:muteki_y_chuzume")),
-						(entity.getPersistentData().getDouble("minecraft_armor_weapon:muteki_z_chuzume")), _ent.getYRot(), _ent.getXRot());
+			if (!_ent.level.isClientSide() && _ent.getServer() != null) {
+				_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(), _ent.level instanceof ServerLevel ? (ServerLevel) _ent.level : null, 4,
+						_ent.getName().getString(), _ent.getDisplayName(), _ent.level.getServer(), _ent), "tp @s ~ ~ ~");
+			}
 		}
 	}
 }
