25e197040c5cf29a626ba406d8462f127f1460ee
0526ngs <78349683+hrmcngs@users.noreply.github.com>
2024-07-09 21:12:17 +0900

    version up

--- src/main/java/minecraftarmorweapon/procedures/KabuseruturuwoShoudeChituteiruJiannoteitukuProcedure.java
+++ src/main/java/minecraftarmorweapon/procedures/KabuseruturuwoShoudeChituteiruJiannoteitukuProcedure.java
@@ -1,6 +1,7 @@
 package minecraftarmorweapon.procedures;
 
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.network.chat.Component;
 
@@ -12,23 +13,25 @@ public class KabuseruturuwoShoudeChituteiruJiannoteitukuProcedure {
 	public static void execute(Entity entity) {
 		if (entity == null)
 			return;
-		if ((entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new MinecraftArmorWeaponModVariables.PlayerVariables())).aaa == 1) {
-			(new ItemStack(MinecraftArmorWeaponModItems.KABUSERU.get())).setHoverName(Component.literal("\u304B\u3076\u308C\u3084"));
-		}
-		if ((entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new MinecraftArmorWeaponModVariables.PlayerVariables())).aaa == 2) {
-			(new ItemStack(MinecraftArmorWeaponModItems.KABUSERU.get())).setHoverName(Component.literal("\u80F4\u306B\u3053\u308C\u7740\u308D"));
-		}
-		if ((entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new MinecraftArmorWeaponModVariables.PlayerVariables())).aaa == 3) {
-			(new ItemStack(MinecraftArmorWeaponModItems.KABUSERU.get())).setHoverName(Component.literal("\u811A\u306B\u3053\u308C\u7740\u308D"));
-		}
-		if ((entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new MinecraftArmorWeaponModVariables.PlayerVariables())).aaa == 4) {
-			(new ItemStack(MinecraftArmorWeaponModItems.KABUSERU.get())).setHoverName(Component.literal("\u8DB3\u306B\u3053\u308C\u7740\u308D"));
-		}
-		if ((entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new MinecraftArmorWeaponModVariables.PlayerVariables())).aaa == 5) {
-			(new ItemStack(MinecraftArmorWeaponModItems.KABUSERU.get())).setHoverName(Component.literal("\u3053\u308C\u6301\u3066\u5229\u304D\u624B\u306B"));
-		}
-		if ((entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new MinecraftArmorWeaponModVariables.PlayerVariables())).aaa == 6) {
-			(new ItemStack(MinecraftArmorWeaponModItems.KABUSERU.get())).setHoverName(Component.literal("\u3053\u308C\u6301\u3066\u5229\u304D\u624B\u3058\u3083\u306A\u3044\u65B9\u306B"));
+		if (MinecraftArmorWeaponModItems.KABUSERU.get() == (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem()) {
+			if ((entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new MinecraftArmorWeaponModVariables.PlayerVariables())).aaa == 1) {
+				(new ItemStack(MinecraftArmorWeaponModItems.KABUSERU.get())).setHoverName(Component.literal("\u304B\u3076\u308C\u3084"));
+			}
+			if ((entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new MinecraftArmorWeaponModVariables.PlayerVariables())).aaa == 2) {
+				(new ItemStack(MinecraftArmorWeaponModItems.KABUSERU.get())).setHoverName(Component.literal("\u80F4\u306B\u3053\u308C\u7740\u308D"));
+			}
+			if ((entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new MinecraftArmorWeaponModVariables.PlayerVariables())).aaa == 3) {
+				(new ItemStack(MinecraftArmorWeaponModItems.KABUSERU.get())).setHoverName(Component.literal("\u811A\u306B\u3053\u308C\u7740\u308D"));
+			}
+			if ((entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new MinecraftArmorWeaponModVariables.PlayerVariables())).aaa == 4) {
+				(new ItemStack(MinecraftArmorWeaponModItems.KABUSERU.get())).setHoverName(Component.literal("\u8DB3\u306B\u3053\u308C\u7740\u308D"));
+			}
+			if ((entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new MinecraftArmorWeaponModVariables.PlayerVariables())).aaa == 5) {
+				(new ItemStack(MinecraftArmorWeaponModItems.KABUSERU.get())).setHoverName(Component.literal("\u3053\u308C\u6301\u3066\u5229\u304D\u624B\u306B"));
+			}
+			if ((entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new MinecraftArmorWeaponModVariables.PlayerVariables())).aaa == 6) {
+				(new ItemStack(MinecraftArmorWeaponModItems.KABUSERU.get())).setHoverName(Component.literal("\u3053\u308C\u6301\u3066\u5229\u304D\u624B\u3058\u3083\u306A\u3044\u65B9\u306B"));
+			}
 		}
 	}
 }
--- src/main/java/minecraftarmorweapon/procedures/RkigaYasaretatokiProcedure.java
+++ src/main/java/minecraftarmorweapon/procedures/RkigaYasaretatokiProcedure.java
@@ -262,6 +262,15 @@ public class RkigaYasaretatokiProcedure {
 							capability.syncPlayerVariables(entity);
 						});
 					}
+				} else if ((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == MinecraftArmorWeaponModItems.KABUSERU.get()) {
+					{
+						String _setval = "\u304B\u3076\u308C\u3084";
+						entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+							capability.ddd = _setval;
+							capability.syncPlayerVariables(entity);
+						});
+					}
+					(new ItemStack(MinecraftArmorWeaponModItems.KABUSERU.get())).setHoverName(Component.literal("\u304B\u3076\u308C\u3084"));
 				} else {
 					{
 						String _setval = "\u56DE\u8EE2\u65AC\u308A";
@@ -281,6 +290,15 @@ public class RkigaYasaretatokiProcedure {
 							capability.syncPlayerVariables(entity);
 						});
 					}
+				} else if ((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == MinecraftArmorWeaponModItems.KABUSERU.get()) {
+					{
+						String _setval = "\u80F4\u306B\u3053\u308C\u7740\u308D";
+						entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+							capability.ddd = _setval;
+							capability.syncPlayerVariables(entity);
+						});
+					}
+					(new ItemStack(MinecraftArmorWeaponModItems.KABUSERU.get())).setHoverName(Component.literal("\u80F4\u306B\u3053\u308C\u7740\u308D"));
 				} else {
 					{
 						String _setval = "\u8599\u304E\u6255\u3044";
@@ -300,6 +318,15 @@ public class RkigaYasaretatokiProcedure {
 							capability.syncPlayerVariables(entity);
 						});
 					}
+				} else if ((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == MinecraftArmorWeaponModItems.KABUSERU.get()) {
+					{
+						String _setval = "\u811A\u306B\u3053\u308C\u7740\u308D";
+						entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+							capability.ddd = _setval;
+							capability.syncPlayerVariables(entity);
+						});
+					}
+					(new ItemStack(MinecraftArmorWeaponModItems.KABUSERU.get())).setHoverName(Component.literal("\u811A\u306B\u3053\u308C\u7740\u308D"));
 				} else {
 					{
 						String _setval = "\u7A81\u304D";
@@ -311,42 +338,18 @@ public class RkigaYasaretatokiProcedure {
 				}
 			}
 			if ((entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new MinecraftArmorWeaponModVariables.PlayerVariables())).aaa == 5) {
-				{
-					String _setval = "\u65AC\u6483";
-					entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
-						capability.ddd = _setval;
-						capability.syncPlayerVariables(entity);
-					});
-				}
-			}
-			if ((entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new MinecraftArmorWeaponModVariables.PlayerVariables())).aaa == 6) {
-				{
-					String _setval = "\u4F55\u3082\u8D77\u304D\u306A\u3044";
-					entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
-						capability.ddd = _setval;
-						capability.syncPlayerVariables(entity);
-					});
-				}
-			}
-			if ((entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new MinecraftArmorWeaponModVariables.PlayerVariables())).aaa >= 7) {
-				{
-					double _setval = 2;
-					entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
-						capability.aaa = _setval;
-						capability.syncPlayerVariables(entity);
-					});
-				}
-				if ((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == MinecraftArmorWeaponModItems.SWORD_OF_NIGHT.get()) {
+				if ((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == MinecraftArmorWeaponModItems.KABUSERU.get()) {
 					{
-						String _setval = "\u00A7d\u307D\u3093\u30D1\u30C1\u30D1\u30C1";
+						String _setval = "\u8DB3\u306B\u3053\u308C\u7740\u308D";
 						entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
 							capability.ddd = _setval;
 							capability.syncPlayerVariables(entity);
 						});
 					}
+					(new ItemStack(MinecraftArmorWeaponModItems.KABUSERU.get())).setHoverName(Component.literal("\u8DB3\u306B\u3053\u308C\u7740\u308D"));
 				} else {
 					{
-						String _setval = "\u56DE\u8EE2\u65AC\u308A";
+						String _setval = "\u65AC\u6483";
 						entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
 							capability.ddd = _setval;
 							capability.syncPlayerVariables(entity);
@@ -354,83 +357,81 @@ public class RkigaYasaretatokiProcedure {
 					}
 				}
 			}
-		} else if ((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == MinecraftArmorWeaponModItems.KABUSERU.get()) {
-			if ((entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new MinecraftArmorWeaponModVariables.PlayerVariables())).aaa == 2) {
-				{
-					String _setval = "\u304B\u3076\u308C\u3084";
-					entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
-						capability.ddd = _setval;
-						capability.syncPlayerVariables(entity);
-					});
-				}
-				(new ItemStack(MinecraftArmorWeaponModItems.KABUSERU.get())).setHoverName(Component.literal("\u304B\u3076\u308C\u3084"));
-			}
-			if ((entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new MinecraftArmorWeaponModVariables.PlayerVariables())).aaa == 3) {
-				{
-					String _setval = "\u80F4\u306B\u3053\u308C\u7740\u308D";
-					entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
-						capability.ddd = _setval;
-						capability.syncPlayerVariables(entity);
-					});
-				}
-				(new ItemStack(MinecraftArmorWeaponModItems.KABUSERU.get())).setHoverName(Component.literal("\u80F4\u306B\u3053\u308C\u7740\u308D"));
-			}
-			if ((entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new MinecraftArmorWeaponModVariables.PlayerVariables())).aaa == 4) {
-				{
-					String _setval = "\u811A\u306B\u3053\u308C\u7740\u308D";
-					entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
-						capability.ddd = _setval;
-						capability.syncPlayerVariables(entity);
-					});
-				}
-				(new ItemStack(MinecraftArmorWeaponModItems.KABUSERU.get())).setHoverName(Component.literal("\u811A\u306B\u3053\u308C\u7740\u308D"));
-			}
-			if ((entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new MinecraftArmorWeaponModVariables.PlayerVariables())).aaa == 5) {
-				{
-					String _setval = "\u8DB3\u306B\u3053\u308C\u7740\u308D";
-					entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
-						capability.ddd = _setval;
-						capability.syncPlayerVariables(entity);
-					});
-				}
-				(new ItemStack(MinecraftArmorWeaponModItems.KABUSERU.get())).setHoverName(Component.literal("\u8DB3\u306B\u3053\u308C\u7740\u308D"));
-			}
 			if ((entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new MinecraftArmorWeaponModVariables.PlayerVariables())).aaa == 6) {
-				{
-					String _setval = "\u3053\u308C\u6301\u3066\u5229\u304D\u624B\u306B";
-					entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
-						capability.ddd = _setval;
-						capability.syncPlayerVariables(entity);
-					});
+				if ((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == MinecraftArmorWeaponModItems.KABUSERU.get()) {
+					{
+						String _setval = "\u3053\u308C\u6301\u3066\u5229\u304D\u624B\u306B";
+						entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+							capability.ddd = _setval;
+							capability.syncPlayerVariables(entity);
+						});
+					}
+					(new ItemStack(MinecraftArmorWeaponModItems.KABUSERU.get())).setHoverName(Component.literal("\u3053\u308C\u6301\u3066\u5229\u304D\u624B\u306B"));
+				} else {
+					{
+						String _setval = "\u4F55\u3082\u8D77\u304D\u306A\u3044";
+						entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+							capability.ddd = _setval;
+							capability.syncPlayerVariables(entity);
+						});
+					}
 				}
-				(new ItemStack(MinecraftArmorWeaponModItems.KABUSERU.get())).setHoverName(Component.literal("\u3053\u308C\u6301\u3066\u5229\u304D\u624B\u306B"));
 			}
-			if ((entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new MinecraftArmorWeaponModVariables.PlayerVariables())).aaa == 7) {
-				{
-					String _setval = "\u3053\u308C\u6301\u3066\u5229\u304D\u624B\u3058\u3083\u306A\u3044\u65B9\u306B";
-					entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
-						capability.ddd = _setval;
-						capability.syncPlayerVariables(entity);
-					});
+			if ((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == MinecraftArmorWeaponModItems.KABUSERU.get()) {
+				if ((entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new MinecraftArmorWeaponModVariables.PlayerVariables())).aaa == 7) {
+					{
+						String _setval = "\u3053\u308C\u6301\u3066\u5229\u304D\u624B\u3058\u3083\u306A\u3044\u65B9\u306B";
+						entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+							capability.ddd = _setval;
+							capability.syncPlayerVariables(entity);
+						});
+					}
+					(new ItemStack(MinecraftArmorWeaponModItems.KABUSERU.get())).setHoverName(Component.literal("\u3053\u308C\u6301\u3066\u5229\u304D\u624B\u3058\u3083\u306A\u3044\u65B9\u306B"));
 				}
-				(new ItemStack(MinecraftArmorWeaponModItems.KABUSERU.get())).setHoverName(Component.literal("\u3053\u308C\u6301\u3066\u5229\u304D\u624B\u3058\u3083\u306A\u3044\u65B9\u306B"));
-			}
-			if ((entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new MinecraftArmorWeaponModVariables.PlayerVariables())).aaa >= 8) {
-				{
-					double _setval = 2;
-					entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
-						capability.aaa = _setval;
-						capability.syncPlayerVariables(entity);
-					});
+				if ((entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new MinecraftArmorWeaponModVariables.PlayerVariables())).aaa >= 8) {
+					{
+						double _setval = 2;
+						entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+							capability.aaa = _setval;
+							capability.syncPlayerVariables(entity);
+						});
+					}
+					{
+						String _setval = "\u304B\u3076\u308C\u3084";
+						entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+							capability.ddd = _setval;
+							capability.syncPlayerVariables(entity);
+						});
+					}
+					(new ItemStack(MinecraftArmorWeaponModItems.KABUSERU.get())).setHoverName(Component.literal("\u304B\u3076\u308C\u3084"));
 				}
-				{
-					String _setval = "\u304B\u3076\u308C\u3084";
-					entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
-						capability.ddd = _setval;
-						capability.syncPlayerVariables(entity);
-					});
+			} else {
+				if ((entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new MinecraftArmorWeaponModVariables.PlayerVariables())).aaa >= 7) {
+					{
+						double _setval = 2;
+						entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+							capability.aaa = _setval;
+							capability.syncPlayerVariables(entity);
+						});
+					}
+					if ((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == MinecraftArmorWeaponModItems.SWORD_OF_NIGHT.get()) {
+						{
+							String _setval = "\u00A7d\u307D\u3093\u30D1\u30C1\u30D1\u30C1";
+							entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+								capability.ddd = _setval;
+								capability.syncPlayerVariables(entity);
+							});
+						}
+					} else {
+						{
+							String _setval = "\u56DE\u8EE2\u65AC\u308A";
+							entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+								capability.ddd = _setval;
+								capability.syncPlayerVariables(entity);
+							});
+						}
+					}
 				}
-				(new ItemStack(MinecraftArmorWeaponModItems.KABUSERU.get())).setHoverName(Component.literal("\u304B\u3076\u308C\u3084"));
 			}
 		} else if ((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == MinecraftArmorWeaponModItems.A_2.get()) {
 			if ((entity.getCapability(MinecraftArmorWeaponModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new MinecraftArmorWeaponModVariables.PlayerVariables())).aaa == 2) {
